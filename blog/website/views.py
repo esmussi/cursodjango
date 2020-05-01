c12e68cf1cf276306046415c2ef2c93dc04cae42
unknown <eduardo.mussi@greencode.xyz>
2020-05-01 08:45:42 -0300

    Aula comandos sistema de template

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def hello_blog(request):
+    lista = [
+        'Django','Python','Git','Html',
+        'Banco de dados','Linux','Nginx','Uwsgi',
+        'Sistem CTL',
+    ]
+    data = {'name': 'Curso de Django 3', 'Lista_tecnologias': lista}
+    return render(request, 'index.html', data)
